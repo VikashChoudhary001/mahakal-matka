c9abf41b5bbc1bbe96ebbf11459fe05058eac00d
Nigam Mishra <itsmetheofficial@gmail.com>
2025-10-12 13:26:07 +0530

    fix

--- src/views/Splash.jsx
+++ src/views/Splash.jsx
@@ -2,7 +2,7 @@ import SplashImage from "../assets/imgs/splash_image_1comp.jpg";
 
 const Splash = () => {
   return (
-    <div className="overflow-hidden relative max-w-[480px] w-full mx-auto h-full bg-[#baf8ff]">
+    <div className="overflow-hidden relative max-w-[480px] w-full mx-auto h-full bg-[#BBFFF9]">
       <img src={SplashImage} alt="Logo" className="h-full w-full object-contain block" />
     </div>
   );
